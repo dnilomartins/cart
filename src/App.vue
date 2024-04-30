73f3e04ddfb9c3c6e43f77d05b7f8cde232e5303
Danilo Martins <d.smartins@icloud.com>
2024-04-30 10:02:45 -0300

    :sparkles: feat: Separar dados de produtos em arquivo externo e importá-los no componente principal

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,7 @@
 <script setup>
 import { reactive, computed } from 'vue'
+import { products } from './products.js'
 
-const products = [
-  {
-    id: 1,
-    name: 'Banana',
-    price: 1,
-    weight: 0.1
-  },
-  {
-    id: 2,
-    name: 'Apple',
-    price: 4,
-    weight: 0.2
-  },
-  {
-    id: 3,
-    name: 'Orange',
-    price: 9,
-    weight: 0.3
-  }
-]
 const cart = reactive([])
 const addItem = (product) => {
   const exists = cart.some((item) => item.product.id === product.id)
---
+++ src/products.js
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Banana',
+    price: 1,
+    weight: 0.1
+  },
+  {
+    id: 2,
+    name: 'Apple',
+    price: 4,
+    weight: 0.2
+  },
+  {
+    id: 3,
+    name: 'Orange',
+    price: 9,
+    weight: 0.3
+  }
+]
